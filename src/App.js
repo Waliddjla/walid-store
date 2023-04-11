e9614cacecdbb7032614422699d4dc025c546fbb
Walid Djellali <walidbibo1997@gmail.com>
2023-04-11 22:58:50 +0100

    error check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Category from './components/Category';
+import { getCategorie, getProduct } from './Fetcher';
 
-function App() {
+
+function App () {
+  const [categorie, setCategorie] = useState( {errorMessage: '', data: []});
+  const [products, setProducts] = useState({errorMessage: '', data: []});
+
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+    const responesOBject = await getCategorie();
+    setCategorie(responesOBject);
+  }
+  fetchData();
+  }, [])
+
+  const handelCategoryClick = id => {
+    const fetchData = async () => {
+      const responesOBject = await getProduct(id);
+      setProducts(responesOBject);
+    }
+    fetchData();
+    }
+
+const renderCategories =() => {
+  return categorie.data.map(c => 
+    <Category key ={c.id} id={c.id} title= {c.title} onCategoryClick = {() => handelCategoryClick(c.id)}></Category>
+    );
+}
+const renderProducts = () => {
+  return products.data.map(p => 
+    <div>{p.title}</div>
+    )
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <header>My Store</header>
+
+
+    <section>
+      <nav>
+      {categorie.errorMessage && <div>Error: {categorie.errorMessage} </div>}
+      {categorie.data && renderCategories()}
+      </nav>
+      <article>
+      <h1>Products</h1>
+      {products.errorMessage && <div>Error: {products.errorMessage} </div>}
+      { products.data && renderProducts() }
+      </article>
+    </section>
+    <footer>
+      footer
+    </footer>
+</React.Fragment>
   );
+
 }
 
 export default App;
--- src/Fetcher.js
+++ src/Fetcher.js
@@ -3,6 +3,9 @@ export const fetcher = async (url) => {
     let responesOBject = {errorMessage: '', data: []};
   try { 
     const response = await fetch(Base_Url + url);
+    if(!response.ok) {
+      throw new Error (`HTTP Error ${response.status}`);
+      }
     const responseData = await response.json();
     responesOBject.errorMessage = '';
     responesOBject.data = responseData;
@@ -11,4 +14,11 @@ catch (err) {
     responesOBject.errorMessage = err.message;
     }
     return responesOBject;
-};
+}
+
+export const getCategorie = () => {
+  return fetcher("/categories");
+}
+export const getProduct = (id) => {
+  return fetcher("/product?catId=" +id);
+}
